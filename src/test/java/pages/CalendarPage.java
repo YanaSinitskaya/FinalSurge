e2a812edeee1962483811a1e811358dc89f0fe89
Yana Sinitskaya <y.sinitskaya11@gmail.com>
2024-02-17 17:54:17 +0300

    Added methods for getting Date in CalendarPage Updated CalendarPage (added Final By, refresh QuickWorkOutIsDisplayed method locator, refresh locators in deleteWorkoutFromCalendar method)

--- src/test/java/pages/CalendarPage.java
+++ src/test/java/pages/CalendarPage.java
@@ -4,16 +4,21 @@ import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 @Log4j2
 public class CalendarPage extends BasePage {
     public CalendarPage(WebDriver driver) {
         super(driver);
     }
-
+    final static Date TODAY_DATE = new Date();
     final String CALENDAR_URL = BASE_URL + "Calendar.cshtml";
     final By QUICK_WORKOUT_BUTTON = By.id("QuickAddToggle");
+    final By OK_BUTTON = By.xpath("//a[@class='btn btn-primary']");
+    final By DELETE_BUTTON_DROPDOWN = By.xpath("//div[@class='dropdown calendarworkout drag-workout-source drag-workout-reorder-target ui-draggable ui-droppable open']" +
+            "//a[contains(@class,'quick-delete') and text()='Delete']");
 
     @Step("Open calendar page")
     public void open() {
@@ -28,17 +33,33 @@ public class CalendarPage extends BasePage {
     }
 
     @Step("Check that visibility of workout element on Calendar")
-    public boolean QuickWorkOutIsDisplayed(String day, String month, String year) {
-        log.info("Quick Workout is displayed on the Calendar page: '{}', '{}', '{}'", day, month, year);
-        return driver.findElement(By.cssSelector(String.format("div.fc-day-content[data-day='%s'][data-month='%s'][data-year='%s']", day, month, year))).isDisplayed();
+    public boolean QuickWorkOutIsDisplayed(String day, String month, String year, String training) {
+        log.info("Quick Workout is displayed on the Calendar page on '{}', '{}', '{}'", day, month, year);
+        return driver.findElement(By.xpath(String.format("//div[@class='fc-day-content' and @data-day='%s' and @data-month='%s' and @data-year='%s']//following-sibling::div[contains(@class, 'fc-event-activity-title') and text()='%s']", day, month, year, training))).isDisplayed();
     }
 
     @Step("Delete Workout from Calendar")
-    public void deleteWorkoutFromCalendar(String date) {
-        log.info("Workout is deleted from the Calendar");
-        driver.findElement(By.cssSelector(String.format("div.dropdown.calendarworkout.drag-workout-source.drag-workout-reorder-target.ui-draggable.ui-droppable[data-date='%s']", date))).click();
-        driver.findElement(By.cssSelector(".quick-delete")).click();
-        driver.findElement(By.cssSelector(".btn.btn-primary")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".calheader")));
+    public void deleteWorkoutFromCalendar(String day, String month, String year, String training) {
+        log.info("Workout '{}' is deleted from the Calendar on date '{}', '{}', '{}'", training, day, month, year);
+        driver.findElement(By.xpath(String.format("//div[@class='fc-day-content' and @data-day='%s' and @data-month='%s' and @data-year='%s']//following-sibling::div[contains(@class, 'fc-event-activity-title') and text()='%s']", day, month, year, training))).click();
+        driver.findElement(DELETE_BUTTON_DROPDOWN).click();
+        driver.findElement(OK_BUTTON).click();
+    }
+
+    @Step("Set date")
+    public static String getTodayDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("M/dd/yyyy");
+        return sdf.format(TODAY_DATE);
+    }
+
+    @Step("Get value of today date")
+    public static String getValueByType(int typeOfDate){
+        int valueToAdd = 0;
+        if (typeOfDate == Calendar.MONTH) {
+            valueToAdd += 1;
+        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(TODAY_DATE);
+        return  String.valueOf(calendar.get(typeOfDate) + valueToAdd);
     }
 }
--- src/test/java/tests/CalendarTest.java
+++ src/test/java/tests/CalendarTest.java
@@ -3,17 +3,20 @@ package tests;
 import dto.QuickAddWorkout;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.CalendarPage;
+
+import java.util.Calendar;
 
 public class CalendarTest extends BaseTest {
 
-    @Test(description = "Add new Quick Workout Description on Calendar", retryAnalyzer = Retry.class)
-    public void addNewQuickWorkout() {
+    @Test(description = "Add Run Quick Workout on Calendar", retryAnalyzer = Retry.class)
+    public void addRunQuickWorkout() {
         QuickAddWorkout newQuickWorkout = QuickAddWorkout.builder()
-                .workoutDate("2/1/2024")
+                .workoutDate(CalendarPage.getTodayDate())
                 .timeOfDay("4:44")
                 .activityType("Run")
                 .workoutName("RunTest")
-                .workoutDescription("Testing this workout")
+                .workoutDescription("Testing Run workout")
                 .distanceNumber("1")
                 .distanceType("mi")
                 .duration("01:20:33")
@@ -35,28 +38,200 @@ public class CalendarTest extends BaseTest {
                 .saveToLibrary(true)
                 .build();
         loginPage.open();
-        loginPage.login("yanasyhar@gmail.com", "Kz2-3B8-utt-kts");
+        loginPage.login(USER, PASSWORD);
         calendarPage.open();
         calendarPage.addQuickWorkoutWithButton();
         quickAddWorkoutPage.fillingFields(newQuickWorkout);
         quickAddWorkoutPage.save();
-        Assert.assertEquals(calendarPage.QuickWorkOutIsDisplayed("1", "2","2024"), true, "The workout isn't created");
+        Assert.assertEquals(calendarPage.QuickWorkOutIsDisplayed(CalendarPage.getValueByType(Calendar.DAY_OF_MONTH),
+                        CalendarPage.getValueByType(Calendar.MONTH),
+                        CalendarPage.getValueByType(Calendar.YEAR), newQuickWorkout.getActivityType()+":"+""+newQuickWorkout.getWorkoutName()),
+                true,
+                "The Run Workout isn't created");
+    }
+
+    @Test(description = "Add Bike Quick Workout on Calendar", retryAnalyzer = Retry.class)
+    public void addBikeQuickWorkout() {
+        QuickAddWorkout newQuickWorkout = QuickAddWorkout.builder()
+                .workoutDate(CalendarPage.getTodayDate())
+                .timeOfDay("4:44")
+                .activityType("Bike")
+                .workoutName("BikeTest")
+                .workoutDescription("Testing Bike workout")
+                .distanceNumber("1")
+                .distanceType("mi")
+                .duration("01:10:10")
+                .paceNumber("02:15")
+                .paceType("min/mi")
+                .howFeel("Good")
+                .perEffort("4 (Moderate)")
+                .postWorkoutNotesResults("Easy")
+
+                .showPlannedDistanceDuration(true)
+                .pDistance("21")
+                .pDistanceType("mi")
+                .pDuration("0:14:05")
+
+                .markAsRace(true)
+                .ageGroupPlace("4")
+                .overallPlace("1")
+
+                .saveToLibrary(true)
+                .build();
+        loginPage.open();
+        loginPage.login(USER, PASSWORD);
+        calendarPage.open();
+        calendarPage.addQuickWorkoutWithButton();
+        quickAddWorkoutPage.fillingFields(newQuickWorkout);
+        quickAddWorkoutPage.save();
+        Assert.assertEquals(calendarPage.QuickWorkOutIsDisplayed(CalendarPage.getValueByType(Calendar.DAY_OF_MONTH),
+                        CalendarPage.getValueByType(Calendar.MONTH),
+                        CalendarPage.getValueByType(Calendar.YEAR), "Bike"),
+                true,
+                "The Bike Workout isn't created");
+    }
+
+    @Test(description = "Add Swim Quick Workout on Calendar", retryAnalyzer = Retry.class)
+    public void addSwimQuickWorkout() {
+        QuickAddWorkout newQuickWorkout = QuickAddWorkout.builder()
+                .workoutDate(CalendarPage.getTodayDate())
+                .timeOfDay("4:44")
+                .activityType("Swim")
+                .workoutName("SwimTest")
+                .workoutDescription("Testing Swim workout")
+                .distanceNumber("1")
+                .distanceType("mi")
+                .duration("01:10:10")
+                .paceNumber("02:15")
+                .paceType("min/mi")
+                .howFeel("Good")
+                .perEffort("1 (Very Light)")
+                .postWorkoutNotesResults("Easy")
+
+                .showPlannedDistanceDuration(true)
+                .pDistance("21")
+                .pDistanceType("mi")
+                .pDuration("0:14:05")
+
+                .markAsRace(true)
+                .ageGroupPlace("4")
+                .overallPlace("1")
+
+                .saveToLibrary(true)
+                .build();
+        loginPage.open();
+        loginPage.login(USER, PASSWORD);
+        calendarPage.open();
+        calendarPage.addQuickWorkoutWithButton();
+        quickAddWorkoutPage.fillingFields(newQuickWorkout);
+        quickAddWorkoutPage.save();
+        Assert.assertEquals(calendarPage.QuickWorkOutIsDisplayed(CalendarPage.getValueByType(Calendar.DAY_OF_MONTH),
+                        CalendarPage.getValueByType(Calendar.MONTH),
+                        CalendarPage.getValueByType(Calendar.YEAR), "Swim"),
+                true,
+                "The Swim Workout isn't created");
+    }
+
+    @Test(description = "Add RecoveryRehab Quick Workout on Calendar", retryAnalyzer = Retry.class)
+    public void addRecoveryRehabQuickWorkout() {
+        QuickAddWorkout newQuickWorkout = QuickAddWorkout.builder()
+                .workoutDate(CalendarPage.getTodayDate())
+                .timeOfDay("4:44")
+                .activityType("Recovery/Rehab")
+                .workoutName("RecoveryRehab test")
+                .workoutDescription("Testing RecoveryRehab workout")
+                .distanceNumber("1")
+                .distanceType("mi")
+                .duration("01:10:10")
+                .paceNumber("02:15")
+                .paceType("min/mi")
+                .howFeel("Good")
+                .perEffort("1 (Very Light)")
+                .postWorkoutNotesResults("Easy")
+
+                .showPlannedDistanceDuration(true)
+                .pDistance("21")
+                .pDistanceType("mi")
+                .pDuration("0:14:05")
+
+                .markAsRace(true)
+                .ageGroupPlace("4")
+                .overallPlace("1")
+
+                .saveToLibrary(true)
+                .build();
+        loginPage.open();
+        loginPage.login(USER, PASSWORD);
+        calendarPage.open();
+        calendarPage.addQuickWorkoutWithButton();
+        quickAddWorkoutPage.fillingFields(newQuickWorkout);
+        quickAddWorkoutPage.save();
+        Assert.assertEquals(calendarPage.QuickWorkOutIsDisplayed(CalendarPage.getValueByType(Calendar.DAY_OF_MONTH),
+                        CalendarPage.getValueByType(Calendar.MONTH),
+                        CalendarPage.getValueByType(Calendar.YEAR), "Recovery/Rehab"),
+                true,
+                "The RecoveryRehab Workout isn't created");
+    }
+
+    @Test(description = "Add StrengthTraining Quick Workout on Calendar", retryAnalyzer = Retry.class)
+    public void addStrengthTrainingQuickWorkout() {
+        QuickAddWorkout newQuickWorkout = QuickAddWorkout.builder()
+                .workoutDate(CalendarPage.getTodayDate())
+                .timeOfDay("7:25")
+                .activityType("Strength Training")
+                .workoutName("StrengthTraining test")
+                .workoutDescription("Testing StrengthTraining workout")
+                .distanceNumber("1")
+                .distanceType("mi")
+                .duration("00:45:55")
+                .paceNumber("02:15")
+                .paceType("min/mi")
+                .howFeel("Poor")
+                .perEffort("10 (Max Effort)")
+                .postWorkoutNotesResults("Easy")
+
+                .showPlannedDistanceDuration(true)
+                .pDistance("21")
+                .pDistanceType("mi")
+                .pDuration("0:14:05")
+
+                .markAsRace(true)
+                .ageGroupPlace("4")
+                .overallPlace("1")
+
+                .saveToLibrary(true)
+                .build();
+        loginPage.open();
+        loginPage.login(USER, PASSWORD);
+        calendarPage.open();
+        calendarPage.addQuickWorkoutWithButton();
+        quickAddWorkoutPage.fillingFields(newQuickWorkout);
+        quickAddWorkoutPage.save();
+        Assert.assertEquals(calendarPage.QuickWorkOutIsDisplayed(CalendarPage.getValueByType(Calendar.DAY_OF_MONTH),
+                        CalendarPage.getValueByType(Calendar.MONTH),
+                        CalendarPage.getValueByType(Calendar.YEAR), "Strength Training"),
+                true,
+                "The StrengthTraining Workout isn't created");
     }
 
     @Test(description = "Remove Quick Workout from Calendar", retryAnalyzer = Retry.class)
     public void deleteWorkoutFromCalendar() {
         QuickAddWorkout newQuickWorkout = QuickAddWorkout.builder()
-                .workoutDate("2/4/2024")
-                .activityType("Bike")
+                .workoutDate(CalendarPage.getTodayDate())
+                .activityType("Cross Training")
 
                 .build();
         loginPage.open();
-        loginPage.login("yanasyhar@gmail.com", "Kz2-3B8-utt-kts");
+        loginPage.login(USER, PASSWORD);
         calendarPage.open();
         calendarPage.addQuickWorkoutWithButton();
         quickAddWorkoutPage.fillingFields(newQuickWorkout);
         quickAddWorkoutPage.save();
-        calendarPage.deleteWorkoutFromCalendar("2/4/2024");
-        Assert.assertEquals(calendarPage.QuickWorkOutIsDisplayed("4", "2","2024"), false, "The workout isn't deleted");
+        calendarPage.deleteWorkoutFromCalendar(CalendarPage.getValueByType(Calendar.DAY_OF_MONTH),
+                CalendarPage.getValueByType(Calendar.MONTH),
+                CalendarPage.getValueByType(Calendar.YEAR), "Cross Training");
+        Assert.assertEquals(calendarPage.QuickWorkOutIsDisplayed(CalendarPage.getValueByType(Calendar.DAY_OF_MONTH),
+        CalendarPage.getValueByType(Calendar.MONTH),
+        CalendarPage.getValueByType(Calendar.YEAR), "Cross Training"), false, "The workout isn't deleted");
     }
 }
